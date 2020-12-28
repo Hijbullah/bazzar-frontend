2f3a5e06bfc3e3a323f92c19a44874c7c92a4fcd
Hijbullah Amin <hijbullaah@gmail.com>
2020-12-28 18:57:08 +0600

    name

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   head: {
-    title: 'frontend',
+    title: 'Bazzar',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
